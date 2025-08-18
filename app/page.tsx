9c0b0f6d7dc04b4297505c277752bb0f358fa009
Devon McGrath <devon.mcgrath@gmail.com>
2025-08-18 15:24:31 +0100

    Add Shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import {Button} from '@/components/ui/button'
+
 export default function Page() {
   return (
     <section>
       <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>My Portfolio</h1>
       <p className='mb-4'>{`This is a simple portfolio page.`}</p>
+      <Button>Click me</Button>
     </section>
   )
 }
