ea2cfe3d0a407ba1894ef260a0a33c415e11b67c
Devon McGrath <devon.mcgrath@gmail.com>
2025-10-16 12:00:33 +0100

    Add Favicon

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className='w-full p-4 bg-gray-800 text-white flex justify-center'>
-      <h1 className='text-2xl font-bold'>My Application Header</h1>
+    <header className='w-full p-4 flex justify-center'>
+      <h1 className='text-2xl font-bold'>Devon McGrath</h1>
     </header>
   )
 }
