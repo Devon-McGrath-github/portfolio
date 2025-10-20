b93355044a1b0bd6b78ed4e8d4fed435f3627bb9
Devon McGrath <devon.mcgrath@gmail.com>
2025-10-20 14:50:47 +0100

    Footer Icons

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+import { Mail, Github, Linkedin } from 'lucide-react'
+
 export default function Footer() {
   return (
     <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
@@ -5,17 +7,23 @@ export default function Footer() {
         href="https://www.linkedin.com/in/devon-mcgrath-17462558/"
         target="_blank"
         rel="noreferrer"
-        className="underline hover:text-blue-500"
+        className="underline hover:text-destructive"
       >
+        <Linkedin className="inline-block mr-2 mb-1" size={16} />
         LinkedIn
       </a>
       <a
         href="https://github.com/Devon-McGrath-github"
         target="_blank"
         rel="noreferrer"
-        className="underline hover:text-blue-500"
+        className="underline hover:text-destructive"
       >
+        <Github className="inline-block mr-2 mb-1" size={16} />
         GitHub
+      </a>
+      <a href="mailto:devon.mcgrath@gmail.com" className="underline hover:text-destructive">
+        <Mail className="inline-block mr-2 mb-1" size={16} />
+        Email
       </a>
     </footer>
   )
