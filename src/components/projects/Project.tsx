1cf993e3fa536dac67bf75753e610b4e130e769f
Devon McGrath <devon.mcgrath@gmail.com>
2025-11-01 15:41:12 +0000

    Add parallax background for accessibility score

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -2,21 +2,33 @@ import Image from 'next/image'
 import { ParallaxSection } from './Parralax'
 import { ProjectItem } from '@/data/projects'
 import Link from 'next/link'
+import { cn } from '@/utils/utils'
 
 type ProjectProps = Omit<ProjectItem, 'id'>
 
-export default function Project({ title, description, imageUrl, altText, projectUrl }: ProjectProps) {
+export default function Project({ title, textVariant, description, imageUrl, altText, projectUrl }: ProjectProps) {
+  const textColorClass = textVariant === 'dark' ? 'text-foreground ' : 'text-white'
+
   return (
-    <div className="m-[1vw] overflow-hidden relative h-[100vh]">
+    <div className={cn('m-[1vw] overflow-hidden relative h-[100vh]', textVariant === 'dark' ? '' : 'bg-black')}>
       <Link href={`${projectUrl}`} className="block relative w-full h-full">
         <ParallaxSection speed={0.5} className="">
           <Image src={imageUrl} alt={altText} className="object-cover" sizes="100vw" fill />
         </ParallaxSection>
 
-        <div className="text-white absolute inset-0 z-[11] flex flex-col justify-end items-start w-1/2 mb-[4vw] ml-[5vw] pb-0 pl-0">
-          <h2 className="mb-[1vw] text-[clamp(2rem,12vw,6.5rem)]">{title}</h2>
+        <div
+          className={cn(
+            'absolute inset-0 z-[11] flex flex-col justify-end items-start w-1/2 mb-[4vw] ml-[5vw] pb-0 pl-0',
+            textColorClass,
+          )}
+        >
+          <h2 role="presentation" className="mb-[1vw] text-[clamp(2rem,12vw,6.5rem)]">
+            {title}
+          </h2>
           <div>
-            <p className="ml-[0.6vw] text-[1.1rem]">{description}</p>
+            <p role="presentation" className="ml-[0.6vw] text-[1.1rem]">
+              {description}
+            </p>
           </div>
         </div>
       </Link>
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,6 +1,7 @@
 export type ProjectItem = {
   id: string
   title: string
+  textVariant?: string
   description: string
   imageUrl: string
   altText: string
@@ -14,14 +15,13 @@ export const projects: ProjectItem[] = [
     description: 'Brand Website',
     imageUrl: '/projects/rinsevodka/rinse-product.jpg',
     altText: 'Rinse Vodka Homepage',
-    // projectUrl: 'https://rinsevodka.com',
   },
   {
     id: 'clean-collective',
     title: 'Clean Collective',
+    textVariant: 'dark',
     description: 'Brand Website',
     imageUrl: '/projects/cleanco/cleanco-home.jpg',
     altText: 'Clean Collective Homepage',
-    // projectUrl: 'https://cleanco.io',
   },
 ]
